e4f31176d52fb3655d322858539bde0672ef264e
Anushka Kishor <kishoranushka96@gmail.com>
2023-11-03 13:12:58 +0530

    third

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ frame.addEventListener("mousemove", function (dets) {
   gsap.to(".frame span", {
     color: "#fff",
     duration: 0.4,
-    y: "-5vw",
+    y: "5vw",
   });
 
   gsap.to(".frame span", {
